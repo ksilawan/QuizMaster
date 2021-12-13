e27c56bd7de08682bbe504d59e9d28c47bcbb1bd
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-12-14 00:51:25 +0800

    first page and log in pages

--- app/src/main/java/com/demo/quizmaster/MainActivity.java
+++ app/src/main/java/com/demo/quizmaster/MainActivity.java
@@ -1,14 +1,26 @@
 package com.demo.quizmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button start;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.startbutton);
+    }
+
+    public void startpage(View view) {
+        Intent nextpage = new Intent(MainActivity.this, LogInPage.class);
+        startActivity(nextpage);
     }
 }
